a13ec0b09721c02872455b03212452a2f3fcbf57
Vishnu <vishnu10kumar11@gmail.com>
2025-02-05 19:11:16 +0530

    onramping

---
+++ apps/bank_webhook/src/index.ts
@@ -0,0 +1,58 @@
+import express from "express";
+import { prisma } from "@repo/db/client";
+
+const app = express();
+
+app.post("/", async (req, res)=>{
+
+    //add secret here. make sure this req is coming from legit way.
+
+    const paymentInformation = {
+        token : req.body.token, 
+        userId: req.body.user_identifier,
+        amount: req.body.amount
+    }
+    
+    try{
+        await prisma.$transaction([
+
+            prisma.balance.update({
+                where:{
+                    userId: Number(paymentInformation.userId)
+                },
+                data:{
+                    amount:{
+                        increment:Number(paymentInformation.amount)
+                    }
+                }
+            }),
+
+            prisma.onRampTransaction.update({
+                where:{
+                    token:paymentInformation.token
+                },
+                data:{
+                    status:"Success"
+                }
+            })
+        ]);
+
+        res.status(200).json({
+            message:"captured"
+        })
+        
+    }catch(e){
+        console.log(e)
+        res.status(411).json({
+            message:"Error while processing bank_webhook. "
+        })
+    }
+    
+
+
+    
+
+    
+
+
+})
